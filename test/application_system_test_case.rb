ca5236e097633a7af67b5b6352ebad555d995457
abeidahmed <abeidahmed92@gmail.com>
2024-02-16 18:43:35 +0400

    feat: setup capybara

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,18 @@
 require "test_helper"
 
+Capybara.always_include_port = true
+Capybara.default_max_wait_time = 5
+Capybara.save_path = Rails.root.join("tmp", "screenshots")
+Capybara.test_id = "data-test-id"
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |option|
+    option.add_argument "no-sandbox"
+  end
+
+  def within_dom_id(record, prefix = nil, &block)
+    within "##{dom_id(record, prefix)}" do
+      block.call
+    end
+  end
 end
